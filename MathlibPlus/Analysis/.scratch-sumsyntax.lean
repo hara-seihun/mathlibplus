167cd1b1ff409af5940daa91d26984ec4794222c
Kenan <kenan@gmktec.local>
2026-08-13 22:25:28 -0400

    Migrate research system to governed NixOS services

--- MathlibPlus/Analysis/.scratch-sumsyntax.lean
+++
@@ -1,4 +0,0 @@
-import MathlibPlus.PrimeMertens
-#check fun (s : Finset Nat) => ∑ n : Nat ∈ s, n
-#check fun (s : Finset Nat) => ∑ n : Nat, n
-#check fun (s : Finset Nat) => ∑ n : Nat ∈ s, (n : Int)
--- MathlibPlus/LinearAlgebra/.scratch-checksum.lean
+++
@@ -1,7 +0,0 @@
-import MathlibPlus.LinearAlgebra.SymmetricZMatrix
-#check Finset.sum_smul
-#check Finset.smul_sum
-#check Finset.sum_mul
-#check Finset.mul_sum
-#check Finset.sum_smul_distrib
-#check Finset.smul_sum
